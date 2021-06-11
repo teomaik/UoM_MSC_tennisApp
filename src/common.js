8e48eb809039583332b1ddd8e57d8b914cd8b5ef
tmaikantis <teomaik19@gmail.com>
2021-06-12 01:29:18 +0300

    added common.js

---
+++ src/common.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(function (error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function (ev) {
+    // Prevent some older browsers from popping the install prompt
+    ev.preventDefault();
+    // Stash the event so it can be triggered later.
+ 
+});
+
+
+var myApp = {};
