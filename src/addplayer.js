f2ffceb7623bdaeac38a55d0be6342d0bd070710
tmaikantis <teomaik19@gmail.com>
2021-06-14 19:26:48 +0300

    Value checks for fields about adding a new player

--- src/addplayer.js
+++ src/addplayer.js
@@ -15,15 +15,38 @@ function submintplayer(etv) {
 
 
     if (insNullOrEmpty(lname) || insNullOrEmpty(fname) || insNullOrEmpty(nation) || insNullOrEmpty(birthD) || insNullOrEmpty(height)) {
-        flag = false;
-    }
-
-    //console.log(lname + " " + fname + " " + nation);
-
-    if (!flag) {
         alert("some fields are empty");
         return;
     }
+
+    const regexBD = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+    if (!birthD.match(regexBD)) {
+        alert("Invalid format for date of birth. Please use DD/MM/YYYY");
+        return;
+    }
+
+    const regexHeight = /^[+-]?\d+(\.\d+)?$/
+    if (!height.match(regexHeight)) {
+        alert("Invalid format for height");
+        return;
+    }
+
+    const regexLetterSpace = /^[a-zA-Z\s]*$/;
+    if (!lname.match(regexLetterSpace)) {
+        alert("Last Name must only contain letters and spaces");
+        return;
+    }
+    if (!fname.match(regexLetterSpace)) {
+        alert("First Name must only contain letters and spaces");
+        return;
+    }
+    if (!nation.match(regexLetterSpace)) {
+        alert("Nationality must only contain letters and spaces");
+        return;
+    }
+
+
+    //console.log(lname + " " + fname + " " + nation);
 
     addPlayerToDB(fname, lname, nation, birthD, height);
     playerSubmited();
