a06d27b7f576d6032cdcc2493b761f68b18f4347
tmaikantis <teomaik19@gmail.com>
2021-06-16 19:42:21 +0300

    index javasxript fixes

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ window.onload = function() {
     populateDropDown();
 }
 
+
+document.getElementById("dropbtn").onclick = function() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+document.getElementById("addpl_btn").onclick = function() {
+    window.location.href = "addplayer.html";
+}
+
 function populateDropDown() {
     db.ref('players').once('value', getPlayers, showError);
 }
